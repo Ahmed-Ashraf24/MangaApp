64ae1c95fc50c057bf0bada6d3fc6f51d5390cb9
Ahmed Ashraf <ahmedashrafahmed2716@gmail.com>
2025-03-01 14:01:31 +0200

    making LogInViewModel and SignUpViewModel to validate the email and changing the method of signing up and loging in from localdatabase (room) to remotedatabase (firestore and FireBaseAuth)

--- app/src/main/java/com/example/mangaapp/presentaion/ViewModels/LogInViewModel.kt
+++ app/src/main/java/com/example/mangaapp/presentaion/ViewModels/LogInViewModel.kt
@@ -5,24 +5,34 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.mangaapp.Data.Models.UserEntity
+import com.example.mangaapp.Data.RepoImp.RemoteLogInImpl
 import com.example.mangaapp.Domain.Entity.UserName
 import com.example.mangaapp.Domain.UseCase.NormalLogInUseCase
+import com.example.mangaapp.Domain.UseCase.RemoteLogInUseCase
+import com.example.mangaapp.Utilities.Constants
 import kotlinx.coroutines.launch
 
 class LogInViewModel :ViewModel() {
     private val _loginState = MutableLiveData<Result<UserName>>()
     val loginState: LiveData<Result<UserName>> = _loginState
-    fun login(email: String, password: String) :String ?{
+    private val _emailState = MutableLiveData<Boolean>()
+    val emailState: LiveData<Boolean> get() = _emailState
 
+    fun login(email: String, password: String) :String ?{
+            val isEmailValid=Constants.isValidEmail(email)
+        if(isEmailValid){
             try {
-                val user = NormalLogInUseCase(email,password).excute()
-                _loginState.value = Result.success(user!!)
+                val user  = RemoteLogInUseCase(email,password).excute()!!
+                _loginState.value = Result.success(user)
                 return user.name
             } catch (e: Exception) {
                 _loginState.value = Result.failure(e)
                 return null
 
-            }
+            }}
+        else{
+            return "Invalid Email"
+        }
     }
     }
 
