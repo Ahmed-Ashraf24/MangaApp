9d6acb183faa8985d0ebb2e37442d946a1d29f3f
Ahmed Ashraf <ahmedashrafahmed2716@gmail.com>
2025-03-01 14:04:07 +0200

    editing userdao function to getUserDAO to improve readability

--- app/src/main/java/com/example/mangaapp/Data/Models/UserDataBase.kt
+++ app/src/main/java/com/example/mangaapp/Data/Models/UserDataBase.kt
@@ -1,6 +1,5 @@
 package com.example.mangaapp.Data.Models
 
-import android.content.Context
 import androidx.room.Database
 import androidx.room.Room.databaseBuilder
 import androidx.room.RoomDatabase
@@ -8,10 +7,9 @@ import com.example.mangaapp.MyApplication
 
 @Database(entities = [UserEntity::class], version = 1)
 abstract class UserDataBase: RoomDatabase() {
-    abstract fun userDAO():userdao
+    abstract fun getUserDAO():userdao
 
     companion object{
-
         private var Instant:UserDataBase?=null
         fun getDataBaseInstant():UserDataBase{
             return Instant?:synchronized(this){
