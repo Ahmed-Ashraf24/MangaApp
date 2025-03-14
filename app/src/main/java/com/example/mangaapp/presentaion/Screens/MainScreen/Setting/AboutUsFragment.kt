7f74a9f438c70019375404d2e79676243c4ba574
Ahmed Ashraf <ahmedashrafahmed2716@gmail.com>
2025-03-14 14:14:24 +0200

    adding some settings pages

--- app/src/main/java/com/example/mangaapp/presentaion/Screens/MainScreen/Setting/AboutUsFragment.kt
+++ app/src/main/java/com/example/mangaapp/presentaion/Screens/MainScreen/Setting/AboutUsFragment.kt
@@ -1,14 +1,11 @@
-package com.example.mangaapp.presentaion.Screens.theMainScreen
+package com.example.mangaapp.presentaion.Screens.MainScreen.Setting
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.mangaapp.databinding.FragmentSettingsBinding
-import com.example.mangaapp.presentaion.Screens.Auth.MainActivity
-import com.example.mangaapp.presentaion.ViewModels.Auth.LogoutViewModel
+import com.example.mangaapp.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,15 +14,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [SettingsFragment.newInstance] factory method to
+ * Use the [AboutUsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SettingsFragment : Fragment() {
+class AboutUsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    lateinit var binding:FragmentSettingsBinding
-    val logoutViewModel= LogoutViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,28 +34,8 @@ class SettingsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        (activity as? MainScreenActivity)!!.binding.editTextSearch.visibility=View.GONE
-
-        binding=FragmentSettingsBinding.inflate(layoutInflater)
-        return binding.root
-    }
-
-    override fun onDestroy() {
-        (activity as? MainScreenActivity)!!.binding.editTextSearch.visibility=View.VISIBLE
-
-        super.onDestroy()
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val user=(activity as? MainScreenActivity)!!.user
-        binding.settingsEmail.text=user.email
-        binding.settingsUsername.text=user.name
-        binding.logout.setOnClickListener {
-            logoutViewModel.logout()
-            val intent= Intent(requireContext(),MainActivity::class.java)
-            startActivity(intent)
-        }
-
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about_us, container, false)
     }
 
     companion object {
@@ -70,12 +45,12 @@ class SettingsFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingsFragment.
+         * @return A new instance of fragment AboutUsFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            SettingsFragment().apply {
+            AboutUsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
