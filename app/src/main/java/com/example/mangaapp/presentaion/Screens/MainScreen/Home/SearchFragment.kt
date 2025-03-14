3f2de11c96cdf1b96ed8f97d30115eb13054c7e3
Ahmed Ashraf <ahmedashrafahmed2716@gmail.com>
2025-03-14 14:12:29 +0200

    applying some changes after adding the search in the home screen and turning the manga page fragment

--- app/src/main/java/com/example/mangaapp/presentaion/Screens/MainScreen/Home/SearchFragment.kt
+++ app/src/main/java/com/example/mangaapp/presentaion/Screens/MainScreen/Home/SearchFragment.kt
@@ -1,26 +1,22 @@
-package com.example.mangaapp.presentaion.Screens.theMainScreen
+package com.example.mangaapp.presentaion.Screens.MainScreen.Home
 
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mangaapp.R
 import com.example.mangaapp.Utilities.Constants
 import com.example.mangaapp.Utilities.UIAdapters.SearchedMangaAdapter
 import com.example.mangaapp.databinding.FragmentSearchBinding
-import com.example.mangaapp.presentaion.Screens.mangaPage.MangaPage
+import com.example.mangaapp.presentaion.Screens.mangaPage.MangaPageFragment
+import com.example.mangaapp.presentaion.Screens.MainScreen.MainScreenActivity
 import com.example.mangaapp.presentaion.ViewModels.MangaAndChaptersViewModel.MangaViewModel
 import com.google.android.material.chip.Chip
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [SearchFragment.newInstance] factory method to
@@ -31,7 +27,7 @@ class SearchFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
     lateinit var mangaViewModel: MangaViewModel
-    lateinit var binding:FragmentSearchBinding
+    lateinit var binding: FragmentSearchBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -40,11 +36,17 @@ class SearchFragment : Fragment() {
         }
     }
 
+    override fun onDestroy() {
+       val mainFragment= parentFragmentManager.findFragmentById(R.id.fragment_container) as MainFragment
+        mainFragment.binding.editTextSearch.text.clear()
+        super.onDestroy()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding=FragmentSearchBinding.inflate(layoutInflater)
+        binding= FragmentSearchBinding.inflate(layoutInflater)
         return binding.root
     }
 
@@ -64,24 +66,19 @@ class SearchFragment : Fragment() {
             }
             binding.chipGroupGenres.addView(chip)
         }
-        mangaViewModel = ViewModelProvider(this).get(MangaViewModel::class.java)
+        mangaViewModel = (activity as? MainScreenActivity)!!.mangaViewModel
         mangaViewModel.searchedMangaList.observe(viewLifecycleOwner){searchedMangaList->
-            binding.searchResultsRecyclerView.layoutManager=LinearLayoutManager(requireContext())
-            binding.searchResultsRecyclerView.adapter=SearchedMangaAdapter(searchedMangaList){
-                    manga->
-                val intent= Intent(requireContext(), MangaPage::class.java)
-                    .apply {
-                        putExtra("Manga Id",manga.id)
-                        putExtra("Manga Name",manga.name)
-                        putExtra("Manga Description",manga.description)
-                        putExtra("Manga Image",manga.imageUrl)
-                        putExtra("Manga genres",manga.genres)
+            binding.searchResultsRecyclerView.layoutManager= LinearLayoutManager(requireContext())
+            binding.searchResultsRecyclerView.adapter=
+                SearchedMangaAdapter(searchedMangaList) { manga ->
+                    (activity as? MainScreenActivity)!!.selectedManga = manga
+                    mangaViewModel.addMangaToHistList(manga)
 
-                    }
-                mangaViewModel.addMangaToHistList(manga)
-
-                startActivity(intent)
-            }
+                    parentFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, MangaPageFragment())
+                        .addToBackStack(null)
+                        .commit()
+                }
 
         }
 
@@ -91,33 +88,33 @@ class SearchFragment : Fragment() {
         mangaViewModel.fetchSearchedManga(mangaTitle = searchedManga)
         mangaViewModel.isLoading.observe(viewLifecycleOwner){isLoading->
             if(isLoading){
-                binding.loadingSpinner.visibility=View.VISIBLE
+                binding.loadingSpinner.visibility= View.VISIBLE
             }
             else{
-                binding.loadingSpinner.visibility=View.GONE
+                binding.loadingSpinner.visibility= View.GONE
 
             }
         }
         mangaViewModel.searchedMangaList.observe(viewLifecycleOwner){searchedMangaList->
-            binding.searchResultsRecyclerView.layoutManager=LinearLayoutManager(requireContext())
-            binding.searchResultsRecyclerView.adapter=SearchedMangaAdapter(searchedMangaList){
-                    manga->
-                val intent= Intent(requireContext(), MangaPage::class.java)
-                    .apply {
-                        putExtra("Manga Id",manga.id)
-                        putExtra("Manga Name",manga.name)
-                        putExtra("Manga Description",manga.description)
-                        putExtra("Manga Image",manga.imageUrl)
-                        putExtra("Manga genres",manga.genres)
+            binding.searchResultsRecyclerView.layoutManager= LinearLayoutManager(requireContext())
+            binding.searchResultsRecyclerView.adapter=
+                SearchedMangaAdapter(searchedMangaList) { manga ->
+                    (activity as? MainScreenActivity)!!.selectedManga = manga
+                    mangaViewModel.addMangaToHistList(manga)
 
-                    }
-                mangaViewModel.addMangaToHistList(manga)
+                    parentFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, MangaPageFragment())
+                        .addToBackStack(null)
+                        .commit()
+                    mangaViewModel.addMangaToHistList(manga)
 
-                startActivity(intent)
-            }
+                }
 
         }
     }
+
+
+
     companion object {
         /**
          * Use this factory method to create a new instance of
